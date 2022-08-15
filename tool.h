915b28b11abe0023e90f2e7142937e2df194b619
GewHare <71759037+GewHare@users.noreply.github.com>
2022-08-15 16:49:16 +0800

    Delete tool.h

--- tool.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Basics.h"
-#include <iostream>
-using namespace std;
-#ifndef TOOL_H_
-#define TOOL_H_
-Letter CreatLetter(char letter,int exponent);
-void Print(Letter L);
-#endif // !TOOL_h_
