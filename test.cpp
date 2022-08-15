2b417fd8c6565107047761ee5594bdf14e32737d
GewHare <71759037+GewHare@users.noreply.github.com>
2022-08-15 14:55:45 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "Basics.h"
+#include "tool.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	Letter L=CreatLetter('a',2);
+	Print(L);
+	cout << endl;
+	system("pause");
+	return 0;
+}
