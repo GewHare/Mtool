31200defe842de5960fdf53e571e49512ca55f66
GewHare <71759037+GewHare@users.noreply.github.com>
2022-08-19 22:40:14 +0800

    Add files via upload
    添加了注释

--- include/Basics.h
+++ include/Basics.h
@@ -3,59 +3,64 @@
 #define _BASICS_H_
 struct Letter
 {
+	//字母结构
 	char letter;
 	int exponent;
 };
 
 struct LNode
 {
+	//节点
 	Letter data;
 	LNode* next;
 };
 class Monomial
 {
 public:
+	//单项式类，实质为一个链表
 	Monomial();
 	~Monomial();
 	int size;
 	int constnum;
 	Letter value;
-	void append(const Letter &data);
-	void remove(int index);
-	void replace(int index, const Letter& data);
-	Letter read(int index) const;
-	bool operator==(const Monomial M) const;
-	Monomial operator+(const Monomial M) const;
-	Monomial operator-(const Monomial M) const;
-	Monomial operator*(const Monomial M) const;
-	Monomial operator/(const Monomial M) const;
+	void append(const Letter &data);				//向单项式最后添加数据data
+	void remove(int index);							//移除下标为index的节点
+	void replace(int index, const Letter& data);	//将index处的数据替换为data
+	Letter read(int index) const;					//读取index处的数据
+	bool operator==(const Monomial M) const;		//判断两个单项式是否为同类项
+	Monomial operator+(const Monomial M) const;		//将两个同类项相加
+	Monomial operator-(const Monomial M) const;		//将两个同类项相减
+	Monomial operator*(const Monomial M) const;		//将两个单项式相乘
+	Monomial operator/(const Monomial M) const;		//将两个单项式相除
 private:
-	LNode* head;
-	LNode* tail;
+	LNode* head;			//指向开始节点的指针
+	LNode* tail;			//指向最后节点的指针
 };
 
 struct MNode
 {
+	//节点
 	Monomial data;
 	MNode* next;
 };
 class Polynomial
 {
+	//多项式类，实质为链表
 public:
 	Polynomial();
 	~Polynomial();
-	int size;
-	void append(const Monomial& data);
-	void remove(int index);
-	void replace(int index, const Monomial& data);
-	Monomial read(int index) const;
-	Polynomial operator+(const Polynomial& P) const;
-	Polynomial operator-(const Polynomial& P) const;
-	Polynomial operator*(const Polynomial& P) const;
-	Polynomial operator/(const Polynomial& P) const;
+	int size;								//多项式项数(链表长度)
+	void append(const Monomial& data);		//在多项式最后添加data单项式
+	void remove(int index);					//删除index位置的节点
+	void replace(int index, const Monomial& data);		//将index位置的节点删除
+	Monomial read(int index) const;			//读取index处的数据
+	Polynomial operator+(const Polynomial& P) const;	//将两个多项式相加
+	Polynomial operator-(const Polynomial& P) const;	//将两个多项式相减
+	Polynomial operator*(const Polynomial& P) const;	//将两个多项式相乘
+	Polynomial operator/(const Polynomial& P) const;	//将两个多项式相除
 private:
-	MNode* head;
-	MNode* tail;
+	MNode* head;			//指向开始节点的指针
+	MNode* tail;			//指向结束节点的指针
 };
 
 class ArgumentMap
--- include/Tool.h
+++ include/Tool.h
@@ -4,8 +4,8 @@
 using namespace std;
 #ifndef _TOOL_H_
 #define _TOOL_H_
-ostream& operator<<(ostream& O, const Letter& L);
-ostream& operator<<(ostream& O, const Monomial& M);
-ostream& operator<<(ostream& O, const Polynomial& P);
-ostream& operator<<(ostream& O, const Function& F);
+ostream& operator<<(ostream& O, const Letter& L);			//输出Letter对象(字母)
+ostream& operator<<(ostream& O, const Monomial& M);			//输出Monomial对象(单项式)
+ostream& operator<<(ostream& O, const Polynomial& P);		//输出Polynomial对象(多项式)
+ostream& operator<<(ostream& O, const Function& F);			//输出Function对象(函数)
 #endif // !_TOOL_H_
